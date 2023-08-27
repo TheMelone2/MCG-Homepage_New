75b999c106cf0aab16daac38082de6493dc8c6da
Jan Luca <90137253+TheMelone2@users.noreply.github.com>
2023-08-27 21:26:04 +0200

    Add Content: "/unsere-schule/digitale-schule" WiP

---
+++ src/app/unsere-schule/digitale-schule/page.tsx
@@ -0,0 +1,18 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Digitale Schule</h1>
+            <section>
+                <h3>Tablet-Klassen</h3>
+                <h4>Wie viele Schüler nutzen ein iPad</h4>
+                <p>Bis jetzt haben wir zwei iPad-Klassen. Zudem benutzen in den anderen Klassen viele Schüler ein eigenes Tablet oder iPad. </p>
+                <p> Darum haben wir auch in fast allen Unterrichtsräumen einen Apple TV.</p>
+                <h4>Wie viele Lehrer benutzen iPads oder Laptops?</h4>
+                <p>Wir haben um die 45 Lehrer und Referendare. Fast alle von ihnen benutzen iPads oder Laptops. Zudem hat fast jeder Unterrichtsraum einen Laptop.</p>
+                <h3>Informatik</h3>
+                <p>Wir haben 4 Informatikräume und außerdem können das Fach Informatik bereits ab der 7. Klasse anbieten.</p>
+                </section>
+        </div>
+    );
+  }
+  
