d69ffc068c2ae49e95d4aaa7b1a0a3eac6e3a943
Jan Luca <90137253+TheMelone2@users.noreply.github.com>
2023-08-28 17:03:41 +0200

    Add Content for landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
 export default function Page() {
-  return <h1>Willkommen!</h1>;
-}
+  return (
+    <div>
+      <title>Herzlich Willkommen - MCG Dallgow-Döberitz</title>
+      <div className="bg-cover bg-center flex items-center justify-center bg-[url('http://marie-curie-gymnasium-dallgow.de/wp-content/uploads/2016/07/PIC_7181_2_3_export.jpg')] z-1 h-64 rounded-lg relative w-full -left-10 -right-10 -top-8">
+        <h2 className="text-3xl text-white font-bold">Herzlich Willkommen auf der Homepage des <br></br>Marie-Curie-Gymnasiums</h2>
+      </div>
+      <section>
+        <h2>Wer sind wir</h2>
+        <p>Die gesamte Schulgemeinschaft des Marie-Curie-Gymnasiums in Dallgow- Döberitz begrüßt Sie recht herzlich auf der Homepage unserer Schule.</p>
+        <p>Seien Sie eingeladen, sich über unsere schöne und moderne Schule zu informieren, sich einen Überblick über die Unterrichtsinhalte und über das Schulleben zu verschaffen.</p>
+        <p>Unser Gymnasium wurde im Schuljahr 2002/2003 mit zwei siebenten Klassen völlig neu gegründet und hat seitdem eine interessante und spannende Schulentwicklung vollzogen.</p>
+        <p>Dem Namen Marie Curie verpflichtet, legen wir einerseits großen Wert auf die Ausbildung in den Fächern Mathematik, Informatik, Naturwissenschaften und Technik. Aus diesem Grund wurde unsere Schule im November des Jahres 2016 in das nationale MINT-EC- Schulnetzwerk aufgenommen.</p>
+        <p>Andererseits stärken wir in einem weiteren Schwerpunkt die Geistes- und Sozialwissenschaften sowie die Sprachen.</p>
+        <p>Praktisch sieht das so aus, dass je nach Anzahl der Klassen im Jahrgang jeweils 2 bis 3 Klassen dem naturwissenschaftlichen Zweig und 2 bis 3 Klassen dem gesell-schaftswissenschaftlich-sprachlichen Zweig angehören. Seit dem Schuljahr 2017/2018 wird jährlich im gesellschaftswissenschaftlich-sprachlichen Zweig eine bilinguale Klasse eingerichtet.</p>
+        <p>Uns liegt ein interessantes und abwechslungsreiches Schulleben stark am Herzen. Ziele dabei sind:</p>
+        <ul>
+          <li>die Entwicklung eines Wir-Gefühls durch starke Identifikation von Schülern, Lehrkräften und Eltern mit ihrer Schule und</li>
+          <li>die Durchführung von schulischen Veranstaltungen, Projekten und Höhepunkten.</li>
+        </ul>
+        <p>Das Patenprojekt, die AG Schulklima, viele weitere Arbeitsgemeinschaften und interessante Kooperationspartner in den Bereichen Wissenschaft und Wirtschaft helfen uns bei der Erfüllung dieser Ziele.</p>
+        <p>Sind Sie neugierig geworden? Das ist gut so, denn viele weitergehende Detailinformationen erwarten Sie auf den nächsten Seiten unserer Homepage.</p>
+        <p>Die Schulgemeinschaft des Marie-Curie-Gymnasiums</p>
+      </section>
+    </div>
+    )}
