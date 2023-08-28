29add66ff3d38649036f381fc49eb904f8927707
Jan Luca <90137253+TheMelone2@users.noreply.github.com>
2023-08-28 17:36:39 +0200

    Fix: Image at landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div>
       <title>Herzlich Willkommen - MCG Dallgow-Döberitz</title>
-      <div className="bg-cover bg-center flex items-center justify-center bg-[url('http://marie-curie-gymnasium-dallgow.de/wp-content/uploads/2016/07/PIC_7181_2_3_export.jpg')] z-1 h-64 rounded-lg relative w-full -left-10 -right-10 -top-8">
+      <div className="bg-cover bg-center flex items-center justify-center bg-[url('http://marie-curie-gymnasium-dallgow.de/wp-content/uploads/2016/07/PIC_7181_2_3_export.jpg')] z-1 h-64 rounded-lg relative -left-10 -mr-20 -top-8">
         <h2 className="text-3xl text-white font-bold">Herzlich Willkommen auf der Homepage des <br></br>Marie-Curie-Gymnasiums</h2>
       </div>
       <section>
