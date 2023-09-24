f71ecf662512b3561136d95649314ec51aec881b
Jan Luca <90137253+TheMelone2@users.noreply.github.com>
2023-09-24 17:45:19 +0200

    Fix: Text in: unsere-schule\digitale-schule

--- src/app/unsere-schule/digitale-schule/page.tsx
+++ src/app/unsere-schule/digitale-schule/page.tsx
@@ -4,13 +4,13 @@ export default function Page() {
             <h1>Digitale Schule</h1>
             <section>
                 <h3>Tablet-Klassen</h3>
-                <h4>Wie viele Schüler nutzen ein iPad</h4>
-                <p>Bis jetzt haben wir zwei iPad-Klassen. Zudem benutzen in den anderen Klassen viele Schüler ein eigenes Tablet oder iPad. </p>
-                <p> Darum haben wir auch in fast allen Unterrichtsräumen einen Apple TV.</p>
-                <h4>Wie viele Lehrer benutzen iPads oder Laptops?</h4>
-                <p>Wir haben um die 45 Lehrer und Referendare. Fast alle von ihnen benutzen iPads oder Laptops. Zudem hat fast jeder Unterrichtsraum einen Laptop.</p>
+                <h4>Wie viele Schüler benutzen ein iPad?</h4>
+                <p>Zurzeit haben wir zwei iPad-Klassen. Auch in den anderen Klassen nutzen viele Schülerinnen und Schüler ein eigenes Tablet oder iPad.</p>
+                <p>Daher haben wir auch in fast allen Klassenzimmern ein Apple TV.</p>
+                <h4>Wie viele Lehrer nutzen iPads oder Laptops?</h4>
+                <p>Wir haben etwa 45 Lehrer und Referendare. Fast alle nutzen iPads oder Laptops. Außerdem hat fast jeder Unterrichtsraum einen Laptop.</p>
                 <h3>Informatik</h3>
-                <p>Wir haben 4 Informatikräume und außerdem können das Fach Informatik bereits ab der 7. Klasse anbieten.</p>
+                <p>Wir verfügen über 4 Informatikräume und sind somit in der Lage, das Fach Informatik bereits ab der 7. Klasse anzubieten.</p>
                 </section>
         </div>
     );
